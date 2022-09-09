f7df5c2e6587a55968d23d5679ef345991e311bd
Timinath <natholwatimileyin@gmail.com>
2022-09-09 11:15:54 +0000

    Task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,17 +2,18 @@
 /**
  * main - print if the number is positive, zero, or negative
  *
- * Description: with main formation print programming is positive , negative or zero 
+ * Description: with main formation print
  * Return: 0
  */
 int main(void)
 {
-	char c;
-	for (c = 'z'; c >= 'a'; c--)
-	{
-		putchar(c);
-	}
+char c;
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
 
 putchar('\n');
+
 return (0);
 }
