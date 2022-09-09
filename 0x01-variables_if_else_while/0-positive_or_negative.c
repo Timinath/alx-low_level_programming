8d498ac1b14f7f13dc1436d7eccdabc0a2a89679
Timinath <natholwatimileyin@gmail.com>
2022-09-09 06:55:26 +0000

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,7 +12,7 @@ int main(void)
 
 int n;
 
-srand (time(0));
+srand(time (0));
 n = rand() - RAND_MAX / 2;
 
 if(n > 0)
@@ -20,6 +20,6 @@ if(n > 0)
 else if(n == 0)
 	printf("%d is zero\n", n);
 else
-	printf("%d is negative\n",n);
+	printf("%d is negative\n", n);
 return (0);
 }
