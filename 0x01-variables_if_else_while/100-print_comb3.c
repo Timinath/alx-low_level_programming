19661f03948378e2e64c2676235a15f5d0817d26
Timinath <natholwatimileyin@gmail.com>
2022-09-09 12:09:28 +0000

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,21 +9,20 @@
  */
 int main(void)
 {
-	int d;
-	for (d = 0; d < 100; d++)
-	{
-		putchar((d / 10) + '0');
-		putchar((d % 10) + '0');
+int d;
+for (d = 0; d < 100; d++)
+{
+putchar((d / 10) + '0');
+putchar((d % 10) + '0');
 		
-		if (d != 99)
-		
-		{
-			putchar(',');
-			putchar(' ');
+if (d != 99)
+{		
+putchar(',');
+putchar(' ');
 
-		}
+}
 
-	}
-	putchar('\n');
-	return (0);
+}
+putchar('\n');
+return (0);
 }
