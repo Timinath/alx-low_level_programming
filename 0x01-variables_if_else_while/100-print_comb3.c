60a27a86c71b19bfe6b57b6ba1c22e704dbc6961
Timinath <natholwatimileyin@gmail.com>
2022-09-10 21:02:59 +0000

    eh

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,18 +13,15 @@ int main(void)
 	int j;
 
 	for (i = 10; i <= 19; i++)
-	
 	{
 		for (j = 10; j <= 19; j++)
 		{
-
 			if ((j % 10) > (i % 10))
 			{
 				putchar((i % 10) + '0');
 				putchar((j % 10) + '0');
 				
 				if (i != 18 || j != 19)
-
 				{
 					putchar(',');
 					putchar(' ');
@@ -33,5 +30,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
