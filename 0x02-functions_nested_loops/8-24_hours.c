f43a41ca3ee3bad9300ce3c602037ecac20ab069
Timinath <natholwatimileyin@gmail.com>
2022-09-13 09:35:20 +0000

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,7 +13,9 @@ void jack_bauer(void)
 		for (j = 0; j < 60; j++)
 			{
 			_putchar(i / 10 + 48);
-			_putchar(i % 10 + 48);																			_putchar(':');																				_putchar(j / 10 + 48);
+			_putchar(i % 10 + 48);
+			_putchar(':');
+			_putchar(j / 10 + 48);
 			_putchar(j % 10 + 48);
 			_putchar('\n');
 			}																			}
