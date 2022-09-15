84e3f63a7e029ac3418a1f483b177f63b5893521
Timinath <natholwatimileyin@gmail.com>
2022-09-15 09:00:03 +0000

    sf

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,15 +7,15 @@
  */
 void print_most_numbers(void)
 {
-	char n = 0;
+	int i;
 
-	while (n <= 9)
+	i = 0;
+	while (i < 10)
 	{
-		if (n != 2 && n != 4)
-		{
-			_putchar('0' + n);
-		}
-		n++;
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+
+		i++;
 	}
 
 	_putchar('\n');
