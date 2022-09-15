6430dcd67fef2ca7f626f90865f205072016f888
Timinath <natholwatimileyin@gmail.com>
2022-09-15 09:09:02 +0000

    gh

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,14 +6,8 @@
  *@c: the ASCII equivalent of the character
  *Return: 1 if uppercase, else 0
  */
+i
 int _isupper(int c)
 {
-		if ((c >= 'A') && (c <= 'Z'))
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
+	return (c >= 'A' && c <= 'Z');
 }
