3ca6786fa9e9a452f00814c50e60ae5e79417f30
Timinath <natholwatimileyin@gmail.com>
2022-09-16 02:48:24 +0000

    aa

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,9 +7,18 @@
 
 void print_line(int n)
 {
-	while (n-- > 0)
+	int i;
+
+	if (n <= 0)
 	{
-		putchar('_');
+		_putchar('\n');
 	}
-	putchar('\n');
+	else
+	{
+	for (i = 1; i <= n; i++)
+	{
+	_putchar('_');
+	}
+	_putchar('\n');
+	}
 }
