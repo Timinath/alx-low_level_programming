2f3c5d332b8c21d94c6050e2d39e5dde55baba68
Timinath <natholwatimileyin@gmail.com>
2022-09-16 02:34:44 +0000

    wo

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - prints a line
+ *
+ * @n: integer to print line
+ */
+void print_line(int n)
+{
+	int i;
+
+	void print_line(int n)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
