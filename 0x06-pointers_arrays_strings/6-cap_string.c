6650386a42f3e3ec3526634c4024a2bfc1d6a380
Timinath <natholwatimileyin@gmail.com>
2022-09-22 11:57:07 +0000

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,30 +4,26 @@
  * @c: var
  * Return: no return value
  */
-char *cap_string(char *c)
+char *cap_string(char *s)
 {
-	int i, j;
-	int cpt = 32;
-	int sprt[] = {'\n', '\t', ',', ';', '.', '!', '?', '"', '(', ')',
-	'{', '}', ' ' };
+	int count = 0, i;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	for (i = 0; c[i] != '\0'; i++)
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
-		if (c[i] >= 'a' && c[i] <= 'z')
+		for (i = 0; i < 13; i++)
 		{
-		c[i] =  c[1] -cpt
+			if (*(s + count) == sep_words[i])
+			{
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+				*(s + (count + 1)) = *(s + (count + 1)) - 32;
+			break;
+			}
 		}
-	
-	cpt = 0;
-	for (j = 0; j <= 13; j++)
-	{
-
-		if (c[i] == sprt[j])
-		{
-			j = 13;
-			cpt = 32;
-		}
+		count++;
 	}
-	}
-	return (c);
+	return (s);
 }
