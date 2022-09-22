f14e814bef6566597cbcd2b3e1932ba515b1fcf6
Timinath <natholwatimileyin@gmail.com>
2022-09-22 06:47:30 +0000

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncat - function declared to print two concatenated strings
+ * @dest: assigned char variable
+ *  @src: assigned char variable
+ *  @n: assigned int variable
+ *  Return: is dest
+ */
+{
+	int l, j;
+
+	l = 0;
+
+	while (dest[l] != '\0')
+	{
+	l++;
+	}
+
+	for (j = 0; j < n; j++)
+	{
+	dest[l + j] = src[j];
+	if (src[j] == '\0')
+	{
+	j = n;
+	}
+	}
+	return (dest);
+}
